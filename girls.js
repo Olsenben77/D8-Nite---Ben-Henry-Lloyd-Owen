2852f7b064ed8481cd614f35b57006320765629a
Ben Olsen <olsenben1992@gmail.com>
2020-01-16 20:27:12 -0800

    html changes

---
+++ girls.js
@@ -0,0 +1,15 @@
+var musicInput;
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + musicInput,
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    "x-rapidapi-key": "199f1e09a9mshdfab23e44228e4fp1b6d38jsn561dce2385d3"
+  }
+};
+
+$.ajax(settings).done(function(response) {
+  console.log(response);
+});
