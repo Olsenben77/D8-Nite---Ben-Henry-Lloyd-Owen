56b617f8d8968db74078bc18a63d990a986f2e2c
Owen Chanthala <owenchanthala@gmail.com>
2020-01-17 20:50:07 -0800

    not done yet

---
+++ results.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var resultsBody = $(".results-body");
+  var nameTag = $("<h3>");
+  var imgTag = $("<img>");
+  var venueTag = $("<h4>");
+  var timeTag = $("<h4>");
+
+  var eventName = localStorage.getItem("eventName");
+  var eventImg = localStorage.getItem("eventImg");
+  var eventVenue = localStorage.getItem("eventVenue");
+  var eventTime = localStorage.getItem("eventTime");
+
+  nameTag.text(eventName);
+  imgTag.attr("src", eventImg);
+  venueTag.text(eventVenue);
+  timeTag.text(eventTime);
+
+  resultsBody.append(nameTag, imgTag, venueTag, timeTag);
+});
